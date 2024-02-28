49754e1440a139c2999439d9f6d76780be642df5
konrad101296 <konradslimak@o2.pl>
2024-02-28 17:03:51 +0100

    Add project files.

---
+++ week 2 lesson 4 exercise nr.5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter your name: ");
+string Name = Console.ReadLine();
+Console.WriteLine("Enter your surname: ");
+string Surname = Console.ReadLine();
+Console.WriteLine("Enter your phone number: ");
+long Phonenubmer = long.Parse(Console.ReadLine());
+Console.WriteLine("Enter your email: ");
+string email = Console.ReadLine();
+Console.WriteLine("Enter your height: ");
+double height = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter your weight: ");
+double weight = double.Parse(Console.ReadLine());
+Console.WriteLine($"Name: {Name}\nSurname: {Surname}\nPhonenumber: {Phonenubmer}\nemail: {email}\nheight: {height}\nweight: {weight}");
+
+
+
