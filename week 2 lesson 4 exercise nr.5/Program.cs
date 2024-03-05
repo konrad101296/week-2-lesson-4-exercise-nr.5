94756a6172d25600b55954aa5eb1aad4feaab0e9
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-05 17:21:59 +0100

    Update Program.cs

--- week 2 lesson 4 exercise nr.5/Program.cs
+++ week 2 lesson 4 exercise nr.5/Program.cs
@@ -1,16 +1,16 @@
 ﻿Console.WriteLine("Enter your name: ");
-string Name = Console.ReadLine();
+string name = Console.ReadLine();
 Console.WriteLine("Enter your surname: ");
-string Surname = Console.ReadLine();
+string surname = Console.ReadLine();
 Console.WriteLine("Enter your phone number: ");
-long Phonenubmer = long.Parse(Console.ReadLine());
+long phoneNubmer = long.Parse(Console.ReadLine());
 Console.WriteLine("Enter your email: ");
 string email = Console.ReadLine();
 Console.WriteLine("Enter your height: ");
 double height = double.Parse(Console.ReadLine());
 Console.WriteLine("Enter your weight: ");
 double weight = double.Parse(Console.ReadLine());
-Console.WriteLine($"Name: {Name}\nSurname: {Surname}\nPhonenumber: {Phonenubmer}\nemail: {email}\nheight: {height}\nweight: {weight}");
+Console.WriteLine($"Name: {name}\nSurname: {surname}\nPhonenumber: {phoneNubmer}\nemail: {email}\nheight: {height}\nweight: {weight}");
 
 
 
